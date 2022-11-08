5d14e022b39ae399d80e4b2079ee37d22c279b7e
Dmitry <dmitrytulay@coherentsolutions.com>
2022-11-08 11:25:25 +0300

    feat: aded tableView with custom Cells

---
+++ BalinaSoftTestTask/BalinaSoftTestTask/Cells/PhotoTypeCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoTypeCell.swift
+//  BalinaSoftTestTask
+//
+//  Created by Dmitry Tulay on 11/8/22.
+//
+
+import UIKit
+
+class PhotoTypeCell: UITableViewCell {
+    
+    @IBOutlet private var typeLable: UILabel!
+    
+    func configureLable() {
+        typeLable.text = "asds"
+    }
+    
+}
+
